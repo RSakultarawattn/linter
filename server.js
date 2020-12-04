e764c08ebc3d56ac1d7a99642dc042240652c8a7
RSakultarawattn <ray.sakultarawattn@gmail.com>
2020-12-04 13:30:35 -0800

    addexpress

--- server.js
+++ server.js
@@ -1,28 +1,17 @@
+
+const { linter } = require('linter.js');
 const express = require('express');
 const app = express();
 
 
-app.use(express.json());
 
+app.use(express.json())
 
-
-app.listen(7890, () => {
-    console.log('started on 7890');
+app.post('/api/v1/lint', (req, res) => {
+  const result = linter(req.body.lint)
+  res.send(result)
 })
 
-const express = require('express');
-const app = express();
-const Color = require('./lib/Color');
-
-app.use(express.json());
-
-// endpoints
-app.post('/api/v1/color', (req, res) => {
-  const color = new Color(req.body.red, req.body.green, req.body.blue);
-  res.send(color.asHex());
-});
-
-// listen
 app.listen(7890, () => {
-  console.log('started on 7890');
-});
+  console.log('started on 7890')
+})
